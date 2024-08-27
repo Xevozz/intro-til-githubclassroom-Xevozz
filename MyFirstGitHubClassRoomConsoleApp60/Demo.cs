68042ccc7bb2ed89b2b0e3cc8ae95533851ffc97
Xevoz_ <Niklas.loev@gmail.com>
2024-08-27 12:55:38 +0200

    tilføjet Demo Class

---
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -0,0 +1,15 @@
+namespace MyFirstGitHubClassRoomConsoleApp60;
+
+public class Demo
+{
+    private string name;
+    private string lastname;
+    private string email;
+    private int alder;
+    
+}
+
+
+
+
+

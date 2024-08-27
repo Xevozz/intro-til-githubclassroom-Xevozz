64912be6020bb033983ff9d9443bc17457cc5743
Xevoz_ <Niklas.loev@gmail.com>
2024-08-27 13:13:24 +0200

    chnages to class

--- MyFirstGitHubClassRoomConsoleApp60/Demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -2,10 +2,10 @@ namespace MyFirstGitHubClassRoomConsoleApp60;
 
 public class Demo
 {
-    private string name;
-    private string lastname;
-    private string email;
-    private int alder;
+    private string name { get; set; }
+    private string lastname { get; set; }
+    private string email { get; set; }
+    private int alder { get; set; }
 
     public Demo(string name, string lastname, string email, int alder)
     {

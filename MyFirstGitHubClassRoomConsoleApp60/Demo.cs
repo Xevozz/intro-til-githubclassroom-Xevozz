99db7f2058a28946116708d2bf53e8bd1328f468
Xevoz_ <Niklas.loev@gmail.com>
2024-08-27 12:57:20 +0200

    tilføj constructor

--- MyFirstGitHubClassRoomConsoleApp60/Demo.cs
+++ MyFirstGitHubClassRoomConsoleApp60/Demo.cs
@@ -6,6 +6,16 @@ public class Demo
     private string lastname;
     private string email;
     private int alder;
+
+    public Demo(string name, string lastname, string email, int alder)
+    {
+        this.name = name;
+        this.lastname = lastname;
+        this.email = email;
+        this.alder = alder;
+    }
+    
+    
     
 }
 
